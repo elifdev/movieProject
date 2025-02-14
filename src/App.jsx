6908eba27d2d3b9b45f937aaa29cef869169e0ad
elifdev <elifdevpro@gmail.com>
2025-02-14 21:37:28 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Search from "./components/Search"
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import {useDebounce} from 'react-use';
-import { updateSearchCount } from "./appwrite";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 const API_BASE_URL='https://api.themoviedb.org/3';
 
@@ -24,6 +24,7 @@ const App = () => {
    const [searchTerm , setSearchTerm] = useState('');
    const [errorMessage, setErrorMessage] = useState('');
    const [movieList, setMovieList] = useState([]);
+   const [trendingMovies, setTrendingMovies] = useState([]);
    const [isLoading, setIsLoading] = useState(false);
    const [debouncedSearchTerm , setDebouncedSearchTerm] = useState()
 
@@ -67,9 +68,27 @@ const App = () => {
      }
    }
 
+   const loadTrendingMovies = async () => {
+    try {
+
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+      
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+     
+    }
+  }
+
+
    useEffect(() => {
       fetchMovies(debouncedSearchTerm);
    }, [debouncedSearchTerm]);
+
+   useEffect(() => {
+    loadTrendingMovies();
+   }, []);
 
   return (
     <main>
@@ -84,9 +103,29 @@ const App = () => {
 
         </header>
 
+        {trendingMovies.length > 0 && (
+
+          <section className="trending">
+
+          <h2>Trending Movies</h2>
+          <ul>
+            {trendingMovies.map((movie,index) => (
+              <li key={movie.$id}>
+                <p>{index + 1}}</p>
+                <img src={movie.poster_url} alt={movie.title} />
+
+              </li>
+
+            ) )}
+          </ul>
+
+          </section>
+
+        )}
+
         <section className="all-movies">
 
-         <h2 className="mt-[20px]">All Movies</h2>
+         <h2>All Movies</h2>
          
          {isLoading ? (
           <Spinner/>
@@ -108,7 +147,7 @@ const App = () => {
       </div>
     </main>
   )
-}
+} 
 
 export default App
 
